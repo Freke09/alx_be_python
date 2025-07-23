318d9af3bbe35511c379f8bc403a23b7321a066a
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-07-23 20:38:14 +0100

    Wrote arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,12 @@
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num1 == 0 or num2 == 0:
+            return "Can not divide by 0"
+        else:
+            return num1 / num2
