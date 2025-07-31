9f468854572dc7419759c0faf08de410ba39efd3
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-07-31 01:02:07 +0100

    Wrote library.py to test with main-1.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title, author, checked_out):
+        self.title = title
+        self.author = author
+        self._is_checked_out = checked_out
+
+
+class Library:
+    def __init__(self, books):
+        if books is None:
+            books = []
+        self._books = books
+    
+    def add_book(self, books):
+        self._books.append(book)
+
+    def check_out_book(self, tittle):
+        for book in self._books:
+            if book.tittle == tittle and book in self.books:
+            self.books -= book
+        else:
+            return f"{book} not available"
+    
+    def return_book(self, book):
+        self.books += book
+        return self.books
+    
+    def list_available_books(self):
+        return self.books
