01ddb30e12ac580573cb96660c59449c0178209d
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-09 05:16:37 +0100

    Modified library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -14,7 +14,7 @@ class EBook(Book):
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
-        self.parge_count = page_count
+        self.page_count = page_count
     
 class Library:
     def __init__(self):
